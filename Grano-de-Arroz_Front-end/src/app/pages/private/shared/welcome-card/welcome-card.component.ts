9409f7a6101aac0618f85a7d1d889c1ffa2e3585
Fefe <Federicomamani1805@gmail.com>
2025-08-20 19:05:40 -0300

    Creacion de componente para el home

---
+++ Grano-de-Arroz_Front-end/src/app/pages/private/shared/welcome-card/welcome-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-welcome-card",
+  standalone: true,
+  templateUrl: "./welcome-card.component.html",
+  styleUrls: ["./welcome-card.component.css"],
+})
+export class WelcomeCardComponent {}
