a466f28648326a0921174147c66be49c14f379e7
Fefe <Federicomamani1805@gmail.com>
2025-08-26 18:13:13 -0300

    Se creo los componentes del home para laboratorio

--- Grano-de-Arroz_Front-end/src/app/pages/private/privateRoutes.ts
+++ Grano-de-Arroz_Front-end/src/app/pages/private/privateRoutes.ts
@@ -5,6 +5,7 @@ import { CustomerComponent } from './main/customer/customer.component';
 import { CustomerRegistrationComponent } from './main/customer-registration/customer-registration.component';
 import { UserComponent } from './main/user/user.component';
 import { UserRegistrationComponent } from './main/user-registration/user-registration.component';
+import { WelcomeLabComponent } from './main/home/home-components/welcome-lab/welcome-lab.component';
 
 export const privateRoutes: Routes = [
   {
